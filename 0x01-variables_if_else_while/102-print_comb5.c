2539b12e58ee2941faf5fc1f83f02874cb66bda5
NGJay <unigwegodwin@gmail.com>
2022-09-10 02:15:09 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combinations of two two-digit numbers,
+ *        ranging from 0-99, separated by a comma followed by a space.
+ *
+ * Return: alway returns 0
+ */
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 <= 99; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 <= 99; digit2++)
+		{
+			putchar(digit1 / 10 + '0');
+			putchar(digit1 % 10 + '0');
+			putchar(' ');
+			putchar(digit2 / 10 + '0');
+			putchar(digit2 % 10 + '0');
+
+			if (!(digit1 == 98 && digit2 == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
